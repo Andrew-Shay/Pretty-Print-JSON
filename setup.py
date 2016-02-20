40417d0428259b31a8ce79fe8d447eb27f4f8422
Andrew Shay <andrew.shay@andrewshay.me>
2016-02-20 17:19:08 -0500

    version bump. Added 3.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pretty_print_json',
-    version='1.0.1',
+    version='1.1.0',
     description='Tool for printing pretty JSON',
     long_description='Tool for printing pretty JSON',
     url='http://andrewshay.me',
@@ -12,6 +12,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'
     ],
     packages=['pretty_print_json'],
     entry_points={
