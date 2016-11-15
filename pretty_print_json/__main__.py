a82c0f3c99376cef8725d88a23f1907c79e2245b
Andrew Shay <andrewshay@shayconcepts.com>
2016-11-14 19:50:19 -0500

    Simplified functions

--- pretty_print_json/__main__.py
+++ pretty_print_json/__main__.py
@@ -5,26 +5,19 @@ import json
 import sys
 
 __author__ = "Andrew Shay"
-__version__ = "1.1.1"
+__version__ = "2.0.0"
 
 
 def get_commands():
     parser = argparse.ArgumentParser(prog='ppj',
-                                     description='Prints pretty JSON')
-    subparsers = parser.add_subparsers()
+                                     description='Pretty Print JSON v{}'.format(__version__))
 
-    p = subparsers.add_parser('p', help='Prints JSON file')
-    p.set_defaults(which='p')
-    p.add_argument('file_loc', help='Location of JSON file')
-    p.add_argument("-o", "--overwrite",
+    parser.add_argument('path', help='Path to JSON file')
+    parser.add_argument("-o", "--overwrite",
                    help="Overwrites original file with pretty JSON",
                    action="store_true")
-    p.add_argument("-f", "--file",
+    parser.add_argument("-f", "--file",
                    help="Write pretty JSON to specific file")
-
-    version_subparser = subparsers.add_parser('version',
-                                              help='Prints version')
-    version_subparser.set_defaults(which='version')
 
     commands = parser.parse_args()
     return commands
@@ -35,44 +28,38 @@ def main():
 
     commands = get_commands()
 
-    if commands.which == 'version':
-        print("v" + __version__)
-        return True
+    try:
+        json_file = open(commands.path, "r").read()
+    except Exception as e:
+        print("[ERROR] Unable to open file")
+        print(e)
+        return 2
 
-    if commands.which == 'p':
+    try:
+        pretty_json = json.dumps(json.loads(json_file), sort_keys=True,
+                                 indent=4, separators=(',', ': '))
+    except Exception as e:
+        print("[ERROR] Invalid JSON")
+        print(e)
+        return 5
 
+    print(pretty_json)
+
+    if commands.overwrite:
         try:
-            json_file = open(commands.file_loc, "r").read()
+            open(commands.path, "w").write(pretty_json)
         except Exception as e:
-            print("[ERROR] Unable to open file")
+            print("[ERROR] Unable to overwrite file")
             print(e)
-            return 2
+            return_code = 3
 
+    if commands.file is not None:
         try:
-            pretty_json = json.dumps(json.loads(json_file), sort_keys=True,
-                                     indent=4, separators=(',', ': '))
+            open(commands.file, "w").write(pretty_json)
         except Exception as e:
-            print("[ERROR] Invalid JSON")
+            print("[ERROR] Unable to write new file")
             print(e)
-            return 1
-
-        print(pretty_json)
-
-        if commands.overwrite:
-            try:
-                open(commands.file_loc, "w").write(pretty_json)
-            except Exception as e:
-                print("[ERROR] Unable to overwrite file")
-                print(e)
-                return_code = 3
-
-        if commands.file is not None:
-            try:
-                open(commands.file, "w").write(pretty_json)
-            except Exception as e:
-                print("[ERROR] Unable to write new file")
-                print(e)
-                return_code = 4
+            return_code = 4
 
     return return_code
 
